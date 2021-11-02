193b739416ad65f8f065771537fdafd9e27e0729
Luca-Dellavalle <62664083+Luca-Dellavalle@users.noreply.github.com>
2021-11-02 23:46:35 +0100

    Correzione ReadMe e scaricamento nuovi esercizi

--- Ese 11 Mongo/server.ts
+++ Ese 11 Mongo/server.ts
@@ -3,8 +3,10 @@ import HEADERS from "./headers.json"
 import {Dispatcher} from "./dispatcher"
 import * as _mongodb from "mongodb"
 const mongoClient = _mongodb.MongoClient
-const port : number = 1337;
+const CONNECTIONSTRING = "mongodb://127.0.0.1:27017";
 
+/*
+const port : number = 1337;
 const dispatcher : Dispatcher =  new Dispatcher();
 
 const server = _http.createServer(function(req, res){
@@ -12,16 +14,17 @@ const server = _http.createServer(function(req, res){
 })
 server.listen(port);
 console.log("Server in ascolto sulla porta " + port);
+*/
 
 //Inserimento di un nuovo record
-mongoClient.connect("mongodb://127.0.0.1:27017",function(err,client){
+mongoClient.connect(CONNECTIONSTRING,function(err,client){
     if(!err){
         let db = client.db("5B_Studenti");
         let collection = db.collection("Studenti");
         let student = {"Nome":"Giovanni","hobbies":["nuoto", "tennis"],"Indirizzo":"Informatica","Cognome":"Puglisi","Sezione":"B","Lavoratore": false, "Residenza":{"Citta":"Genola", "Provincia":"Cuneo","CAP":"12045"}};
         collection.insertOne(student,function(err,data){
             if(!err){
-                console.log(data);
+                console.log("INSERT ",data);
             }
             else{
                 console.log("Errore esecuzione query " + err.message);
@@ -35,13 +38,13 @@ mongoClient.connect("mongodb://127.0.0.1:27017",function(err,client){
 })
 
 //Modello di accesso al database 
-mongoClient.connect("mongodb://127.0.0.1:27017",function(err,client){
+mongoClient.connect(CONNECTIONSTRING,function(err,client){
     if(!err){
         let db = client.db("5B_Studenti");
         let collection = db.collection("Studenti");
         collection.find().toArray(function(err, data){
             if(!err){
-                console.log(data);
+                console.log("FIND ",data);
             }
             else{
                 console.log("Errore esecuzione query " + err.message);
@@ -54,4 +57,42 @@ mongoClient.connect("mongodb://127.0.0.1:27017",function(err,client){
     }
 })
 
+// UPDATEONE (filtro, azione)
+mongoClient.connect(CONNECTIONSTRING,function(err,client){
+    if(!err){
+        let db = client.db("5B_Studenti");
+        let collection = db.collection("Studenti");
+        collection.updateOne({"Nome":"Mario"} , {$set:{"Residenza":"Fossano"}},(function(err, data){
+            if(!err){
+                console.log("UPDATEONE ",data);
+            }
+            else{
+                console.log("Errore esecuzione query " + err.message);
+            }
+            client.close();
+        }));
+    }
+    else{
+        console.log("Errore della connessione al databse " + err.message);
+    }
+})
 
+// DELETEMANY 
+mongoClient.connect(CONNECTIONSTRING,function(err,client){
+    if(!err){
+        let db = client.db("5B_Studenti");
+        let collection = db.collection("Studenti");
+        collection.deleteMany({"Residenza":"Fossano"}, (function(err, data){
+            if(!err){
+                console.log("DELETE ",data);
+            }
+            else{
+                console.log("Errore esecuzione query " + err.message);
+            }
+            client.close();
+        }));
+    }
+    else{
+        console.log("Errore della connessione al databse " + err.message);
+    }
+})
