0bdb1066e42d3414872d9fb6331efae065205cd2
Luca-Dellavalle <62664083+Luca-Dellavalle@users.noreply.github.com>
2021-12-01 09:56:03 +0100

    modifiche server.ts app.use e app.get

--- Express/Ese 01 Express/server.ts
+++ Express/Ese 01 Express/server.ts
@@ -1,24 +1,86 @@
 import express from "express";
-import * as _http from "http";
+import * as fs from "fs";
+import * as http from "http";
+import * as body_parser from "body-parser";
+import { inherits } from "util";
 import HEADERS from "./headers.json";
 
 let port : number = 1337;
 let app = express();
 
-let server = _http.createServer(app);
+let server = http.createServer(app);
 
 server.listen(port,function(){
     console.log("Server in ascolto sulla porta " + port)
+    init();
 });
 
-//elenco delle routes(listeners)
-app.use("*",function(req, res, next){
+let paginaErrore="";
+function init(){
+    fs.readFile("./static/error.html",function(err, data){
+        if(!err){
+            paginaErrore = data.toString();
+        }
+        else{
+            paginaErrore = "<h2>Risorsa non trovata</h2>";
+        }
+    });
+}
+
+
+//****************************************************************
+//elenco delle routes di tipo middleware
+//****************************************************************
+// 1.log 
+app.use("/",function(req, res, next){
     console.log("---->" + req.method + ":"+ req.originalUrl);
     next();
 });
 
-app.get("*", function(req, res, next){
-    res.send("this is the respons");
+// 2.static route
+//il next lo fa automaticamente quando non trova la risorsa
+app.use("/", express.static("./static"));
+
+// 3.route lettura parametri post
+app.use("/", body_parser.json());
+app.use("/", body_parser.urlencoded({"extended":true}));
+
+// 4.log parametri
+app.use("/", function(req, res, next){
+    if(Object.keys(req.query).length > 0){
+        console.log("Parametri GET: ",req.query);
+    }
+    if(Object.keys(req.body).length > 0){
+        console.log("Parametri BODY: ",req.body);
+    }
+    next();
+})
+
+
+//****************************************************************
+//elenco delle routes di risposta al client
+//****************************************************************
+app.get("/api/risorsa1", function(req, res, next){
+    let nome = req.query.nome;
+    res.send({"nome":nome});
+})
+
+app.post("/api/risorsa1", function(req, res, next){
+    let nome = req.body.nome;
+    res.send({"nome":nome});
+})
+
+//****************************************************************
+//default route(risorse non trovate) e route di gestione degli errori
+//****************************************************************
+app.use("/", function(req, res, next){
+    res.status(404);
+    if(req.originalUrl.startsWith("/api/")){
+        res.send("Servizio non trovato");
+    }
+    else{
+        res.send(paginaErrore);
+    }
 })
 
 
