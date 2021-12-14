943161d9ca96fffdfeaba4a0d2c91bca5ff829c1
Luca-Dellavalle <62664083+Luca-Dellavalle@users.noreply.github.com>
2021-12-14 08:51:44 +0100

    primo servizio getCollections implementato

--- Express/Ese 02 CRUD server/static/index.js
+++ Express/Ese 02 CRUD server/static/index.js
@@ -2,18 +2,31 @@
 
 $(document).ready(function() {
 
-    let divIntestazione = $("#divIntestazione")
-    let divCollections = $("#divCollections")
-    let table = $("#mainTable")
-    let divDettagli = $("#divDettagli")
-    let currentCollection = "";
+  let divIntestazione = $("#divIntestazione")
+  let divCollections = $("#divCollections")
+  let table = $("#mainTable")
+  let divDettagli = $("#divDettagli")
+  let currentCollection = "";
 
-    let request = inviaRichiesta("get", "/api/getCollections");
-    request.fail(errore)
-    request.done(function(collections) {
-      console.log(collections);
-    })
+  let request = inviaRichiesta("get", "/api/getCollections");
+  request.fail(errore)
+  request.done(function(collections) {
+    console.log(collections);
+    let label = divCollections.children("label");
+    for (const collection of collections) {
+      let clone = label.clone();
+      clone.appendTo(divCollections);
+      clone.children("input").val(collection.name);
+      clone.children("span").text(collection.name);
+      divCollections.append("<br>");
+    }
+    label.remove();
+  })
 
+  divCollections.on("click","input[type=radio]",function(){
+    
+  });
+    
 
    
    
